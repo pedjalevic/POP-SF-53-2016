b2699910c9f0fc85ac180e27d4aef2407591b414
Predrag Levic <pedjalevic@gmail.com>
2017-11-24 16:32:52 +0100

    Uradjen izgled log in-a i Serializacija

--- POP-SF-53-2016-GUI/POP-SF-53-2016-GUI/Model/ProdajaNamestaja.cs
+++ POP-SF-53-2016-GUI/POP-SF-53-2016-GUI/Model/ProdajaNamestaja.cs
@@ -14,7 +14,6 @@ namespace POP_SF_53_2016_GUI.Model
         public int BrojRacuna { get; set; }
         public String Kupac { get; set; }
         public double PDV { get; set; }
-        public List<DodatnaUsluga> DodatneUsluge { get; set; }
         public double UkupanIznos { get; set; }
     }
 }
---
+++ POP-SF-53-2016-GUI/POP-SF-53-2016-GUI/Utils/GenericSerializer.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System;
+
+namespace POP_SF_53_2016_GUI.Utils
+{
+    class GenericSerializer
+    {
+        public static void Serialize<T>(string fileName, ObservableCollection<T> listToSerialize) where T : class
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(ObservableCollection<T>));
+                using (var sw = new StreamWriter($@"../../Data/{fileName}"))
+                {
+                    serializer.Serialize(sw, listToSerialize);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+        public static ObservableCollection<T> Deserialize<T>(string fileName) where T : class
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(ObservableCollection<T>));
+                using (var sr = new StreamReader($@"../../Data/{fileName}"))
+                {
+                    return (ObservableCollection<T>)serializer.Deserialize(sr);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
